83ebd0f2060bffd45fbae729997987bcae1d608e
saputhebeast <sapumalwijekoon4@gmail.com>
2023-11-23 01:06:28 +0530

    Feat: DTOs

---
+++ Dtos/CharacterRequestDto.cs
@@ -0,0 +1,12 @@
+﻿namespace _net.Dtos
+{
+    public class CharacterRequestDto
+    {
+        public string Name { get; set; } = "Fredo";
+        public int HitPoints { get; set; } = 100;
+        public int Strength { get; set; } = 10;
+        public int Defence { get; set; } = 10;
+        public int Intelligence { get; set; } = 10;
+        public RpgClass Class { get; set; } = RpgClass.Knight;
+    }
+}
---
+++ Dtos/CharacterResponseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace _net.Dtos
+{
+    public class CharacterResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "Fredo";
+        public int HitPoints { get; set; } = 100;
+        public int Strength { get; set; } = 10;
+        public int Defence { get; set; } = 10;
+        public int Intelligence { get; set; } = 10;
+        public RpgClass Class { get; set; } = RpgClass.Knight;
+    }
+}
