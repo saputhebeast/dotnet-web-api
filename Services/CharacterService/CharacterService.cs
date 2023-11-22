9878f21cffa06842b9834faf7c7c0a81727ed34d
saputhebeast <sapumalwijekoon4@gmail.com>
2023-11-22 11:38:48 +0530

    Fix: Possible ArgumentNullException

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -15,7 +15,10 @@
 
         public Character GetCharacterById(int id)
         {
-            return characters.FirstOrDefault(c => c.Id == id);
+            var character = characters.FirstOrDefault(c => c.Id == id);
+            if (character is not null)
+                return character;
+            throw new Exception("Character not found");
         }
 
         public List<Character> AddCharacter(Character newCharacter)
