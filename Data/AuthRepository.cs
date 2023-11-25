cdb4a8f25a72fdf48f9c8d8b8156785fac996d6b
saputhebeast <sapumalwijekoon4@gmail.com>
2023-11-25 19:43:14 +0530

    Feat: Authentication repository implementation

---
+++ Data/AuthRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace _net.Data
+{
+    public class AuthRepository: IAuthRepository
+    {
+        public Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<string>> Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UserExists(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Data/IAuthRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace _net.Data
+{
+    public interface IAuthRepository
+    {
+        Task<ServiceResponse<int>> Register(User user, string password);
+        Task<ServiceResponse<string>> Login(string username, string password);
+        Task<bool> UserExists(string username);
+    }
+}
