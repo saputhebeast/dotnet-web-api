9a99935af3dbc074610cd6fc53b6ae7d86f2903e
saputhebeast <sapumalwijekoon4@gmail.com>
2023-11-25 20:00:04 +0530

    Feat: User registration

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -2,9 +2,25 @@
 {
     public class AuthRepository: IAuthRepository
     {
-        public Task<ServiceResponse<int>> Register(User user, string password)
+        private readonly DataContext _context;
+        
+        public AuthRepository(DataContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
+        }
+        
+        public async Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+            
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            var response = new ServiceResponse<int>();
+            response.Data = user.Id;
+            return response;
         }
 
         public Task<ServiceResponse<string>> Login(string username, string password)
@@ -15,6 +31,15 @@
         public Task<bool> UserExists(string username)
         {
             throw new NotImplementedException();
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
     }
 }
