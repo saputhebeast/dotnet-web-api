76876f70e83b75379d1124b70bd125a4f1e1317a
saputhebeast <sapumalwijekoon4@gmail.com>
2023-11-25 19:09:27 +0530

    Feat: User and character relationship

--- Models/Character.cs
+++ Models/Character.cs
@@ -9,4 +9,5 @@ public class Character
     public int Defence { get; set; } = 10;
     public int Intelligence { get; set; } = 10;
     public RpgClass Class { get; set; } = RpgClass.Knight;
+    public User? User { get; set; }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -3,8 +3,9 @@
     public class User
     {
         public int Id { get; set; }
-        public String Username { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
         public byte[] PasswordHash { get; set; } = Array.Empty<byte>();
         public byte[] PasswordSalt { get; set; } = Array.Empty<byte>();
+        public List<Character>? Characters { get; set; }
     }
 }
