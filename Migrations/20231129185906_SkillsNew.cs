d0815eef149fc6cc0169ceef3a3b32008f004e34
saputhebeast <sapumalwijekoon4@gmail.com>
2023-11-30 00:48:36 +0530

    Feat: Migrations

---
+++ Migrations/20231129185906_SkillsNew.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace net.Migrations
+{
+    /// <inheritdoc />
+    public partial class SkillsNew : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Characters_Skills_SkillId",
+                table: "Characters");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Characters_SkillId",
+                table: "Characters");
+
+            migrationBuilder.DropColumn(
+                name: "SkillId",
+                table: "Characters");
+
+            migrationBuilder.CreateTable(
+                name: "CharacterSkill",
+                columns: table => new
+                {
+                    CharactersId = table.Column<int>(type: "int", nullable: false),
+                    SkillsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CharacterSkill", x => new { x.CharactersId, x.SkillsId });
+                    table.ForeignKey(
+                        name: "FK_CharacterSkill_Characters_CharactersId",
+                        column: x => x.CharactersId,
+                        principalTable: "Characters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CharacterSkill_Skills_SkillsId",
+                        column: x => x.SkillsId,
+                        principalTable: "Skills",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CharacterSkill_SkillsId",
+                table: "CharacterSkill",
+                column: "SkillsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CharacterSkill");
+
+            migrationBuilder.AddColumn<int>(
+                name: "SkillId",
+                table: "Characters",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Characters_SkillId",
+                table: "Characters",
+                column: "SkillId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Characters_Skills_SkillId",
+                table: "Characters",
+                column: "SkillId",
+                principalTable: "Skills",
+                principalColumn: "Id");
+        }
+    }
+}
