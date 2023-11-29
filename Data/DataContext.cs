1460991ca023e080658e0087393000b48e6f44df
saputhebeast <sapumalwijekoon4@gmail.com>
2023-11-29 23:35:55 +0530

    Feat: Skills data seeding

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -6,6 +6,15 @@
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Skill>().HasData(
+                new Skill { Id = 1, Name = "Fireball", Damage = 30 },
+                new Skill { Id = 2, Name = "Frenzy", Damage = 20 },
+                new Skill { Id = 3, Name = "Blizzard", Damage = 50 }
+            );
+        }
+
         public DbSet<Character> Characters => Set<Character>();
         public DbSet<User> Users => Set<User>();
         public DbSet<Weapon> Weapons => Set<Weapon>();
